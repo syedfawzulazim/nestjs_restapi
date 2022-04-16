44513be7874ce971ea91af06eb1b6c6e721b940b
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-17 01:25:50 +0200

    app module added

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { BooksModule } from './books/books.module';
+import { UserModule } from './users/user.module';
+
+import { AuthModule } from './auth/auth.module';
+import { PurchaseModule } from './purchase/purchase.module';
+
+@Module({
+  imports: [
+    BooksModule,
+    AuthModule,
+    UserModule,
+    PurchaseModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://adnansparrow:asdasdx@cluster01.b7yso.mongodb.net/BookkeepingDB?retryWrites=true&w=majority',
+    ),
+    PurchaseModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
